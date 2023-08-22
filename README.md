77f7f131b3ff9422900da6217fb5ecb652cc9e5f
FreePhoenix888 <freephoenix888@gmail.com>
2023-08-22 16:08:35 +0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+[![npm](https://img.shields.io/npm/v/@freephoenix888/recursive-object-map.svg)](https://www.npmjs.com/package/@freephoenix888/recursive-object-map) 
+[![Gitpod](https://img.shields.io/badge/Gitpod-ready--to--code-blue?logo=gitpod)](https://gitpod.io/#https://github.com/FreePhoenix888/recursive-object-map.git) 
+
+Map your object recursively
+
---
+++ src/main.ts
@@ -0,0 +1,47 @@
+export type RecusriveObjectMapCallback = (key: string, value: any) => { newKey: string, newValue: any };
+
+/**
+ * Maps an object recursively
+ * 
+ * @example
+```ts
+const myObject = {
+  foo: 'bar',
+  baz: {
+    qux: 'quux',
+    corge: {
+      grault: 'garply',
+    }
+  }
+};
+
+const result = await recursiveObjectMap(myObject, (key, value) => {
+  return {
+    newKey: `My Key: ${key}`,
+    newValue: typeof value === 'string' ? `My Value: ${value}` : value,
+  }
+}
+console.log({result})
+// {
+//   "My Key: foo": "My Value: bar",
+//   "My Key: baz": {
+//     "My Key: qux": "My Value: quux",
+//     "My Key: corge": {
+//       "My Key: grault": "My Value: garply"
+//     }
+//   }
+// }
+```
+ */
+export async function recursiveObjectMap(obj: Record<string, any>, mapFn: RecusriveObjectMapCallback): Promise<Record<string, any>> {
+  const result: Record<string, any> = {};
+  for (const [key, value] of Object.entries(obj)) {
+    const { newKey, newValue } = mapFn(key, value);
+    if (typeof value === 'object' && value !== null && !Array.isArray(value)) {
+      result[newKey] = await recursiveObjectMap(newValue, mapFn);
+    } else {
+      result[newKey] = newValue;
+    }
+  }
+  return result;
+}
